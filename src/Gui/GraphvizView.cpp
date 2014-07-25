8710f66e8bb8e98e53ff57ae3142f274a49c1580
Michael Ang <mangtronix@gmail.com>
2014-07-25 22:38:19 +0200

    Set default path for Graphviz on OSX to /usr/local/bin (directory used by Homebrew)

--- src/Gui/GraphvizView.cpp
+++ src/Gui/GraphvizView.cpp
@@ -72,6 +72,8 @@ QByteArray GraphvizView::exportGraph(const QString& format)
 
 #ifdef FC_OS_LINUX
     QString path = QString::fromUtf8(hGrp->GetASCII("Graphviz", "/usr/bin").c_str());
+#ifdef FC_OS_MACOSX
+    QString path = QString::fromUtf8(hGrp->GetASCII("Graphviz", "/usr/local/bin").c_str());
 #else
     QString path = QString::fromUtf8(hGrp->GetASCII("Graphviz").c_str());
 #endif
